fc6132202ffff234502d10a635a605371923ae37
Ripred100 <60947523+Ripred100@users.noreply.github.com>
2022-06-21 14:16:39 -0400

    Utils WIP

--- utils.c
+++ utils.c
@@ -8,12 +8,12 @@ m2 is unchanged.
 
 Returns: Adress of m1.
 */
-Matrix * m_add(Matrix * m1, Matrix * m2)
+Matrix *m_add(Matrix *m1, Matrix *m2)
 {
-    if(m1->cols == m2->cols && m1->rows == m2->rows)
+    if (m1->cols == m2->cols && m1->rows == m2->rows)
     {
-        //Structure of Matrix stype is flat (1D). Adition can be done elementwise
-        for(int i = 0; i < m1->cols * m1->rows; i++)
+        // Structure of Matrix stype is flat (1D). Adition can be done elementwise
+        for (int i = 0; i < m1->cols * m1->rows; i++)
         {
             m1->data[i] = m1->data[i] + m2->data[i];
         }
@@ -27,40 +27,34 @@ Matrix * m_add(Matrix * m1, Matrix * m2)
 
         exit(2);
     }
-    
-
 }
 
 /*
 Function m_multiply
 ---------------
-takes pointer to 2 matricies, m1 and m2 (Defined in matrix.h), and preforms matrix multiplication. 
+takes pointer to 2 matricies, m1 and m2 (Defined in matrix.h), and preforms matrix multiplication.
 
 Returns: pointer to new matrix
 */
-Matrix * m_multiply(Matrix * m1, Matrix * m2)
+Matrix *m_multiply(Matrix *m1, Matrix *m2)
 {
-    if(m1->cols == m2->rows)
+    if (m1->cols == m2->rows)
     {
-        Matrix * mSol = m_create(m1->rows, m2->cols);
+        Matrix *mSol = m_create(m1->rows, m2->cols);
         m_fill(mSol, 0);
 
-        
-        for(int i = 0; i < mSol->cols; i++)
+        for (int i = 0; i < mSol->cols; i++)
         {
-            
-            for(int j = 0; j < mSol->rows; j++)
-            {
-                //For each loop of i and j, mSol<i,j> is being filled.
 
-                for(int itter = 0; itter < m1->cols; itter++)
+            for (int j = 0; j < mSol->rows; j++)
+            {
+                // For each loop of i and j, mSol<i,j> is being filled.
+
+                for (int itter = 0; itter < m1->cols; itter++)
                 {
                     mSol->data[DIM(mSol->cols, i, j)] += (m1->data[DIM(m1->cols, i, itter)]) * (m2->data[DIM(m2->cols, itter, j)]);
                 }
-
-                
             }
-            
         }
         return mSol;
     }
@@ -74,12 +68,12 @@ Matrix * m_multiply(Matrix * m1, Matrix * m2)
     }
 }
 
-Matrix * m_subtract(Matrix * m1, Matrix * m2)
+Matrix *m_subtract(Matrix *m1, Matrix *m2)
 {
-    if(m1->cols == m2->cols && m1->rows == m2->rows)
+    if (m1->cols == m2->cols && m1->rows == m2->rows)
     {
-        //Structure of Matrix stype is flat (1D). Subtraction can be done elementwise
-        for(int i = 0; i < m1->cols * m1->rows; i++)
+        // Structure of Matrix stype is flat (1D). Subtraction can be done elementwise
+        for (int i = 0; i < m1->cols * m1->rows; i++)
         {
             m1->data[i] = m1->data[i] - m2->data[i];
         }
@@ -93,13 +87,91 @@ Matrix * m_subtract(Matrix * m1, Matrix * m2)
 
         exit(2);
     }
-    
 }
-Matrix * m_dot(Matrix * m1, Matrix * m2)
+
+// DO NOT USE. WIP
+Matrix *m_dot(Matrix *m1, Matrix *m2)
 {
-    
+    if (m1->cols == m2->rows == 1)
+    {
+        Matrix *mSol = m_create(m1->rows, m2->cols);
+        m_fill(mSol, 0);
+
+        for (int i = 0; i < mSol->cols; i++)
+        {
+
+            for (int j = 0; j < mSol->rows; j++)
+            {
+                // For each loop of i and j, mSol<i,j> is being filled.
+
+                for (int itter = 0; itter < m1->cols; itter++)
+                {
+                    mSol->data[DIM(mSol->cols, i, j)] += (m1->data[DIM(m1->cols, i, itter)]) * (m2->data[DIM(m2->cols, itter, j)]);
+                }
+            }
+        }
+        return mSol;
+    }
+    else
+    {
+        printf("ERROR: Dimension mismatch in m_multiply \n");
+        m_print(m1);
+        m_print(m2);
+
+        exit(2);
+    }
 }
-// Matrix * m_apply(MAT_t (*func)(MAT_t), Matrix * m);
-// Matrix * m_scale(MAT_t n, Matrix * m);
-// Matrix * m_addBias(MAT_t n, Matrix * m);
-// Matrix * m_transpose(Matrix * m);
+
+/*
+Function m_apply
+----------------
+Takes a pointer to a function *func that takes an argument of type MAT_t and returns a variable of type MAT_t, and a pointer to a matrix *m.
+It calls the function on each element of matrix m.
+
+Returns: pointer to matrix m
+*/
+Matrix *m_apply(MAT_t (*func)(MAT_t), Matrix *m)
+{
+    for (int i = (m->cols * m->rows) - 1; i >= 0; i--)
+    {
+        m->data[i] = func(m->data[i]);
+    }
+    return m;
+}
+
+Matrix *m_scale(MAT_t n, Matrix *m)
+{
+    for (int i = (m->cols * m->rows) - 1; i >= 0; i--)
+    {
+        m->data[i] = (m->data[i]) * n;
+    }
+    return m;
+}
+
+Matrix *m_addBias(MAT_t n, Matrix *m)
+{
+    for (int i = (m->cols * m->rows) - 1; i >= 0; i--)
+    {
+        m->data[i] = (m->data[i]) + n;
+    }
+    return m;
+}
+
+// DO NOT USE. WIP
+Matrix *m_transpose(Matrix *m)
+{
+
+    for (int i = 0; i < mSol->cols; i++)
+    {
+
+        for (int j = 0; j < mSol->rows; j++)
+        {
+            // For each loop of i and j, mSol<i,j> is being filled.
+
+            for (int itter = 0; itter < m1->cols; itter++)
+            {
+                mSol->data[DIM(mSol->cols, i, j)] += (m1->data[DIM(m1->cols, i, itter)]) * (m2->data[DIM(m2->cols, itter, j)]);
+            }
+        }
+    }
+}
