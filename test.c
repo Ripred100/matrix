726a6447fcde0678cefb76818581bbef5b7d0a30
jmutton2 <jmutton2@uwo.ca>
2022-06-21 21:43:49 -0400

    Fixed transpose function

--- test.c
+++ test.c
@@ -3,22 +3,30 @@
 #include "utils.h"
 #include <time.h>
 
-int verifyAlgo(void (*algo)())
+int verifyAlgo()
 {
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
 
+            print("New test for matrix size %dx%d", i, j);
+
             // Create a matrix of varying size
             Matrix *ptr = m_create(i, j);
             m_fill(ptr, 9);
 
+            printf("Matrix Before: ");
+            m_print(ptr);
+
             clock_t t;
             t = clock();
-            m_transpose(ptr);
+            Matrix *t_ptr = m_transpose(ptr);
             t = clock() - t;
             double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
+
+            printf("Matrix Before: ");
+            m_print(ptr);
 
             printf("transpose took %f seconds to execute \n", time_taken);
 
